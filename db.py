ed1cb8234a2a4491be09635bb3e61f31a5ac73fb
Sergey Savelyev <sergey2304s@mailru>
2024-08-18 15:41:33 +0300

    updated db name

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
 
-DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/mydatabase"
+DATABASE_URL = "postgresql+asyncpg://user:password@db:5432/citiesdb"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(
